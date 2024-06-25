71db6390c010d63e048b25a9de97f48c10b32f89
Błażej Bryła <blazej.bryla@ageno.pl>
2024-06-25 13:14:28 +0200

    changed video to background video component

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,17 @@ import {FirstSection, SecondSection, ServicesSection} from '../constants/FrontPa
 import SectionCenterTitle from '@/components/SectionCenterTitle'
 import Services from '@/components/Services'
 import HomeVideo from '@/public/assets/videos/home1.mp4'
-import Video from 'next-video'
+import BackgroundVideo from 'next-video/background-video'
 
 
 const Page = () => {
   return (
     <>
       <header className={'relative'}>
-        <Video controls={false} loop autoPlay src={HomeVideo}/>
+        <BackgroundVideo loop className={'min-h-[300px] '} src={HomeVideo}/>
         <div className={'absolute top-0 flex h-full w-full flex-col items-center justify-center'}>
-          <h1 className={'gradient-text text-center text-5xl uppercase'}>Visage dental studio <br/> of excellence</h1>
-          <p className={'mt-6 text-xl capitalize tracking-wide  text-lightPink'}>Where art meets technology</p>
+          <h1 className={'gradient-text text-center font-heading font-bold uppercase md:text-5xl'}>Visage dental studio <br/> of excellence</h1>
+          <p className={'mt-2 capitalize tracking-wide text-lightPink md:mt-6  md:text-xl'}>Where art meets technology</p>
         </div>
       </header>
       <main>
