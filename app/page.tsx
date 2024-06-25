529fb28ac3960901bcb49d2520e3f1d69da53491
Błażej Bryła <blazej.bryla@ageno.pl>
2024-06-25 13:02:50 +0200

    created home hero section

--- app/page.tsx
+++ app/page.tsx
@@ -2,33 +2,45 @@ import TextWithImage from '@/components/TextWithImage'
 import {FirstSection, SecondSection, ServicesSection} from '../constants/FrontPageData'
 import SectionCenterTitle from '@/components/SectionCenterTitle'
 import Services from '@/components/Services'
+import HomeVideo from '@/public/assets/videos/home1.mp4'
+import Video from 'next-video'
 
 
 const Page = () => {
   return (
-    <main>
-      Main container
-      <TextWithImage 
-        title={FirstSection.title} 
-        text={FirstSection.text} 
-        image={FirstSection.image}
-        alt={'Passion section image'}
-        buttonText={'Our services'}
-        href={'/services'}
-      />
-      <TextWithImage
-        title={SecondSection.title}
-        text={SecondSection.text}
-        image={SecondSection.image}
-        alt={'Dental tech section image'}
-        className={'md:flex-row-reverse'}
-        dark={true}
-      />
-      <div className={'py-24'}>
-        <SectionCenterTitle title={ServicesSection.title} subtitle={ServicesSection.subtitle} href={ServicesSection.href} />
-        <Services items={ServicesSection.items}/>
-      </div>
-    </main>
+    <>
+      <header className={'relative'}>
+        <Video controls={false} loop autoPlay src={HomeVideo}/>
+        <div className={'absolute top-0 flex h-full w-full flex-col items-center justify-center'}>
+          <h1 className={'gradient-text text-center text-5xl uppercase'}>Visage dental studio <br/> of excellence</h1>
+          <p className={'mt-6 text-xl capitalize tracking-wide  text-lightPink'}>Where art meets technology</p>
+        </div>
+      </header>
+      <main>
+        <TextWithImage
+          title={FirstSection.title}
+          text={FirstSection.text}
+          image={FirstSection.image}
+          alt={'Passion section image'}
+          buttonText={'Our services'}
+          href={'/services'}
+        />
+        <TextWithImage
+          title={SecondSection.title}
+          text={SecondSection.text}
+          image={SecondSection.image}
+          alt={'Dental tech section image'}
+          className={'md:flex-row-reverse'}
+          dark={true}
+        />
+        <div className={'py-24'}>
+          <SectionCenterTitle title={ServicesSection.title} subtitle={ServicesSection.subtitle}
+            href={ServicesSection.href}/>
+          <Services items={ServicesSection.items}/>
+        </div>
+      </main>
+    </>
+
   )
 }
 export default Page
